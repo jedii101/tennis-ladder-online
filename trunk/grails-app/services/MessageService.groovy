3dd4f266ef1eea02192b696922b735bdfabad9c6
will.han <will.han@d5e251e5-c1dd-ef47-aacf-8b5f56c3ed3e>
2010-04-21 03:46:18 +0000

    # 34 design

---
+++ trunk/grails-app/services/MessageService.groovy
@@ -0,0 +1,27 @@
+class MessageService{
+    static transactional = true
+    def sendChallenge={
+
+    }
+
+    def listUnResponsedDefenders={
+
+    }
+
+    def acceptChallenge={
+
+    }
+
+    def denyOtherChallenges={
+
+    }
+
+    def cancelChallenges={
+        
+    }
+
+    def listMessages={
+        
+    }
+
+}
